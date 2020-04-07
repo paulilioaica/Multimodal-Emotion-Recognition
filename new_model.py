5dab532fdf878144f64326d546c31325ebcae422
paulilioaica <ipaulbeniamin@gmail.com>
2020-04-08 01:40:50 +0300

    Added attn

--- new_model.py
+++ new_model.py
@@ -14,25 +14,6 @@ class GRU(nn.Module):
         x = self.gru(x, h)
         return x
 
-class Kinect_simple(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(kernel_size=4, padding=0, in_channels=3, out_channels=8, stride=2, bias=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=8),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(kernel_size=4, padding=0, in_channels=8, out_channels=16, bias=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=16),
-            nn.MaxPool2d(kernel_size=2))
-        self.linear = nn.Linear(in_features=8464, out_features=512)
-    def forward(self, x):
-        x = self.network(x)
-        x = self.linear(x.reshape(x.shape[0], -1))
-        x = F.relu(x)
-        return x
-
 
 
 class Kinect(nn.Module):
@@ -58,6 +39,15 @@ class Video(nn.Module):
     def forward(self, x):
         x = self.network(x)
         return x.reshape(x.shape[0], -1)
+
+
+class Attention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(in_features=384, out_features=384)
+    def forward(self, x):
+        x = self.linear(x)
+        return F.softmax(x)
 
 
 class Audio(nn.Module):
@@ -88,16 +78,16 @@ class Classifier(nn.Module):
         self.audio = Audio()
         self.gru_video = GRU()
         self.gru_kinect = GRU()
-        self.kinect = Kinect_simple()
+        self.kinect = Kinect()
         self.linear = nn.Linear(in_features=384, out_features=7)
 
     def forward(self, audio, video_arr, motion_arr):
         if torch.cuda.is_available():
-            h_video = torch.zeros((video_arr.shape[0], 128)).cuda()
-            h_kinect = torch.zeros((video_arr.shape[0], 128)).cuda()
+            h_video = torch.zeros((video_arr.shape[0], self.gru_video.hidden_size)).cuda()
+            h_kinect = torch.zeros((video_arr.shape[0], self.gru.hidden_size)).cuda()
         else:
-            h_video = torch.zeros((video_arr.shape[0], 128))
-            h_kinect = torch.zeros((video_arr.shape[0], 128))
+            h_video = torch.zeros((video_arr.shape[0], self.gru_video.hidden_size))
+            h_kinect = torch.zeros((video_arr.shape[0], self.gru_kinect.hidden_size))
         for i in range(video_arr.shape[1]):
             video_seq = self.video(video_arr[:, i, :, :])
             h_video = self.gru_video(video_seq, h_video)
@@ -106,5 +96,7 @@ class Classifier(nn.Module):
             h_kinect = self.gru_kinect(kinect_seq, h_kinect)
         audio = self.audio(audio)
         x = torch.cat([audio, h_video, h_kinect], dim=1)
+        weights = self.attn(x)
+        x = x * weights
         x = self.linear(x)
         return x
