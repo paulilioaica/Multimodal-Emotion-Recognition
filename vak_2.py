4d2450a88c8d24930643dce7f158d356e0bf6b0f
paulilioaica <ipaulbeniamin@gmail.com>
2020-04-10 20:25:56 +0300

    Fix commit

--- vak_2.py
+++ vak_2.py
@@ -1,31 +1,29 @@
-import itertools
-
+import torch.nn.functional as F
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torchvision.models import resnet18
-import numpy as np
 
 
-class GRU(nn.Module):
+class LSTM(nn.Module):
     def __init__(self):
         super().__init__()
-        self.hidden_size = 1024
-        self.gru = nn.GRUCell(input_size=1024+128, hidden_size=self.hidden_size)
+        self.hidden_size = 768
+        self.gru = nn.LSTMCell(input_size=1152, hidden_size=self.hidden_size)
 
     def forward(self, x, h):
         x = self.gru(x, h)
         return x
 
+class Resnet():
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(*list(resnet18(pretrained=True).children())[:-1])
 
 
 class Kinect(nn.Module):
     def __init__(self):
         super().__init__()
-        self.network = nn.Sequential(*list(resnet18(pretrained=True).children())[:-1])
-        for params in self.network.parameters():
-            params.requires_grad = False
-
+        self.network = Resnet().network
 
     def forward(self, x):
         x = self.network(x)
@@ -35,14 +33,12 @@ class Kinect(nn.Module):
 class Video(nn.Module):
     def __init__(self):
         super().__init__()
-        self.network = nn.Sequential(*list(resnet18(pretrained=True).children())[:-1])
-        for params in self.network.parameters():
-            params.requires_grad = False
+        self.network = Resnet().network
+        self.network.eval()
 
     def forward(self, x):
         x = self.network(x)
         return x.reshape(x.shape[0], -1)
-
 
 
 class Audio(nn.Module):
@@ -57,8 +53,7 @@ class Audio(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(num_features=8),
             nn.MaxPool2d(kernel_size=2))
-        self.linear = nn.Linear(in_features=2288, out_features=128)
-        self.linear.requires_grad=False
+        self.linear = nn.Linear(in_features=1360, out_features=128)
 
     def forward(self, x):
         x = self.network(x)
@@ -71,28 +66,23 @@ class Classifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.video = Video()
-        self.gru = GRU()
+        self.lstm = LSTM()
         self.audio = Audio()
         self.kinect = Kinect()
-        self.linear = nn.Linear(in_features=1024, out_features=7)
-        self.combo = list(itertools.product([i for i in range(40)],[i for i in range(20)]))
+        self.linear = nn.Linear(in_features=768, out_features=7)
 
     def forward(self, audio, video_arr, motion_arr):
         if torch.cuda.is_available():
-            h = torch.zeros((video_arr.shape[0], self.gru.hidden_size)).cuda()
+            h = torch.rand((video_arr.shape[0], self.lstm.hidden_size)).cuda()
+            c = torch.rand((video_arr.shape[0], self.lstm.hidden_size)).cuda()
         else:
-            h = torch.zeros((video_arr.shape[0], self.gru.hidden_size))
+            h = torch.zeros((video_arr.shape[0], self.lstm.hidden_size))
+            c = torch.rand((video_arr.shape[0], self.lstm.hidden_size))
         audio = self.audio(audio)
-        for i in range(motion_arr.shape[1]*2):
-            if np.random.uniform(0,1) > 0.5:
-                j,k = self.combo[np.random.randint(0, len(self.combo)-1)]
-                video_seq = self.video(video_arr[:, k, :, :])
-                kinect_seq = self.kinect(motion_arr[:, j, :, :].transpose(1,3))
-            else:
-                video_seq = self.video(video_arr[:, int(i / 4), :, :])
-                kinect_seq = self.kinect(motion_arr[:, int(i/2), :, :].transpose(1,3))
+        for i in range(motion_arr.shape[1]):
+            video_seq = self.video(video_arr[:, int(i/2), :, :])
+            kinect_seq = self.kinect(motion_arr[:,i, :, :].transpose(1, 3))
             x = torch.cat([audio, video_seq, kinect_seq], dim=1)
-            h = self.gru(x, h)
-
+            h, c = self.lstm(x, (h, c))
         x = self.linear(h)
         return x
